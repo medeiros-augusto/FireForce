45dc109969022622bb787f055e9776fd59aeef21
medeiros-augusto <augustomedeiros047@gmail.com>
2023-09-25 09:45:37 -0300

    amem

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use('/assets', express.static('assets'))
 app.use('/images', express.static('images'))
 app.use('/pages', express.static('pages'))
 
+//Conexão com o banco de dados SQL
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: 'root',
-    database: 'FireForce',
+    database: 'fireforce',
 });
-
+//Verifica conexão
 connection.connect(function(err) {
     if (!err) {
         console.log("Conexão como o Banco realizada com sucesso!!!");
@@ -25,9 +26,41 @@ connection.connect(function(err) {
     }
 });
 
+//get para pagina inicial
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
+
+//Porta ao qual o servidor vai rodar
 app.listen(3010, () => {
     console.log('Servidor rodando na porta 3010!')
 })
+
+//Login
+app.post('/login', (req, res) => {
+    let username = req.body.nomelogin;
+    let password = req.body.senhalogin;
+
+    connection.query("SELECT * FROM usuario where email_usuario = '" + username + "'", function (err, rows, fields) {
+        console.log("Results:", rows);
+        if (!err) {
+            if (rows.length > 0) {
+
+                if (rows[0].senha_usuario === password) {
+                    connection.query("SELECT saldo_usuario FROM usuario where email_usuario = '" + username + "'", function (error, results, fields) {
+                        if (error) throw error;
+                        res.send('vai se foder')
+                    });
+                } else {
+                    res.send('Senha incorreta');
+                }
+
+            } else {
+                res.send('Login Falhou - Usuário não cadastrado');
+            }
+        } else {
+            console.log("Erro: Consulta não realizada", err);
+            res.send('Login failed');
+        }
+    });
+});
