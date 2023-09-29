0d763e2c7d522cd9ca1e5906d1a2bc10cddf70e7
medeiros-augusto <augustomedeiros047@gmail.com>
2023-09-29 12:22:14 -0300

    bugado kkk

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const mysql = require('mysql2');
+const ejs = require('ejs');
+
+require('dotenv').config({ path: './secret.env' });
+
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const path = require('path')
 app.use('/assets', express.static('assets'))
 app.use('/images', express.static('images'))
-app.use('/pages', express.static('pages'))
+app.use('/views', express.static('views'))
 
 //Conexão com o banco de dados SQL
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'firecool',
 });
 //Verifica conexão
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (!err) {
         console.log("Conexão como o Banco realizada com sucesso!!!");
     } else {
@@ -26,32 +32,29 @@ connection.connect(function(err) {
     }
 });
 
-//get para pagina inicial
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
 
-//Porta ao qual o servidor vai rodar
-app.listen(3010, () => {
-    console.log('Servidor rodando na porta 3010!')
-})
+app.set('view engine', 'ejs');
 
 //Login
 app.post('/login', (req, res) => {
     let username = req.body.nomelogin;
     let password = req.body.senhalogin;
-
     connection.query("SELECT * FROM usuario where nome_usuario = '" + username + "'", function (err, rows, fields) {
-        console.log("Results:", rows);
         if (!err) {
             if (rows.length > 0) {
-
                 if (rows[0].senha_usuario === password) {
-                    res.send("VAI SE FODER")
+                    req.session.user = {
+                        username: rows[0].nome_usuario,
+                    };
+                    res.redirect('/logadoMEC');
                 } else {
                     res.send('Login Falhou - Senha incorreta');
                 }
-
             } else {
                 res.send('Login Falhou - Usuário não cadastrado');
             }
@@ -61,3 +64,19 @@ app.post('/login', (req, res) => {
         }
     });
 });
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/indexMEC.html')
+})
+
+app.get('/logadoMEC', (req, res) => {
+    if (!req.session.user) {
+      return res.redirect('/'); // Redirecione para a página de login se o usuário não estiver autenticado
+    }
+    res.render('logadoMEC', { user: req.session.user });
+  });
+
+//Porta ao qual o servidor vai rodar
+app.listen(3010, () => {
+    console.log('Servidor rodando na porta 3010!')
+})
