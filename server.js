e98cada2f41024ecb3e312459bd4eac74bbe0bba
medeiros-augusto <augustomedeiros047@gmail.com>
2023-10-01 00:31:32 -0300

    server atualizado

--- server.js
+++ server.js
@@ -1,82 +1,63 @@
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
-const mysql = require('mysql2');
-const ejs = require('ejs');
+const mysql = require('mysql2')
 
-require('dotenv').config({ path: './secret.env' });
-
+const port = 3010;
+var path = require('path')
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({secret:'bf2buy3b2vu22'}))
+app.use(bodyParser.urlencoded({extended:true}))
 
-const path = require('path')
-app.use('/assets', express.static('assets'))
-app.use('/images', express.static('images'))
-app.use('/views', express.static('views'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, '/views'))
 
-//Conexão com o banco de dados SQL
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'firecool',
 });
-//Verifica conexão
+
 connection.connect(function (err) {
     if (!err) {
-        console.log("Conexão como o Banco realizada com sucesso!!!");
+        console.log("Conexão como o Banco realizada com sucesso!!!")
     } else {
-        console.log("Erro: Conexão NÃO realizada", err);
+        console.log("Erro: Conexão NÃO realizada", err)
     }
 });
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-}));
-
-app.set('view engine', 'ejs');
-
-//Login
-app.post('/login', (req, res) => {
-    let username = req.body.nomelogin;
-    let password = req.body.senhalogin;
-    connection.query("SELECT * FROM usuario where nome_usuario = '" + username + "'", function (err, rows, fields) {
+app.post('/', (req,res)=>{
+    connection.query("SELECT * FROM usuario where nome_usuario = '" + req.body.nomelogin + "'", function (err, rows, fields) {
         if (!err) {
             if (rows.length > 0) {
-                if (rows[0].senha_usuario === password) {
-                    req.session.user = {
-                        username: rows[0].nome_usuario,
-                    };
-                    res.redirect('/logadoMEC');
+                if (rows[0].senha_usuario === req.body.senhalogin) {
+                    req.session.login = req.body.nomelogin
+                    res.render('logadoMEC', {login: req.body.nomelogin})
                 } else {
-                    res.send('Login Falhou - Senha incorreta');
+                    res.render('indexMEC')
                 }
             } else {
-                res.send('Login Falhou - Usuário não cadastrado');
+                res.send('Login Falhou - Usuário não cadastrado')
             }
         } else {
-            console.log("Erro: Consulta não realizada", err);
-            res.send('Login failed');
+            console.log("Erro: Consulta não realizada", err)
+            res.send('Login failed')
         }
-    });
-});
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/indexMEC.html')
+    })
 })
 
-app.get('/logadoMEC', (req, res) => {
-    if (!req.session.user) {
-      return res.redirect('/'); // Redirecione para a página de login se o usuário não estiver autenticado
+app.get('/', (req,res)=>{
+    if (req.session.login){
+        res.render('logadoMEC')
+    }else{
+        res.render('indexMEC')
     }
-    res.render('logadoMEC', { user: req.session.user });
-  });
+})
 
-//Porta ao qual o servidor vai rodar
-app.listen(3010, () => {
-    console.log('Servidor rodando na porta 3010!')
+app.listen(port, ()=>{
+    console.log(`Servidor Rodando na porta ${port}`)
 })
