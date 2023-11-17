491cd8ed7d8a61480f341964aefa7c7efc16caf9
Jpvassoler <joaovassoler18@icloud.com>
2023-11-17 20:21:59 -0300

    Gestacional

---
+++ public/scripts/Gestacioal.js
@@ -0,0 +1,28 @@
+//---------Gestacional---------
+
+  // Função para exibir a Anamnese
+  function showAnamnese() {
+    var divAnamnese = document.getElementById('anamnese');
+    divAnamnese.style.display = 'block';
+}
+
+// Função para ocultar a Anamnese
+function hideAnamnese() {
+    var divAnamnese = document.getElementById('anamnese');
+    divAnamnese.style.display = 'none';
+}
+
+// Ocultar a Anamnese inicialmente
+hideAnamnese();
+
+//---------Aparecer Gestante---------
+
+function showGestante() {
+    var divGestante = document.getElementById('Gestante');
+    divGestante.style.display = 'block';
+}
+
+function hideGestante() {
+    var divGestante = document.getElementById('Gestante');
+    divGestante.style.display = 'none';
+}
